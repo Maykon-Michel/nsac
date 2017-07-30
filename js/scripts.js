de619a1877c8b8880594063608c3fd639e283ed5
Maykon-Michel <maykoncti@gmail.com>
2017-07-30 10:40:21 -0300

    1.17.05.07.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
 
+function changeColor(color) {
+	switch(color) {
+		case 'blue':
+			primary_color = "#000033";
+			secondary_color = "#000055";
+			tertiary_color = "#AACCFF";
+			break;
+		case 'gray':
+			primary_color = "#333333";
+			secondary_color = "#555555";
+			tertiary_color = "#CCCCCC";
+			break;
+		case 'purple':
+			primary_color = "#330033";
+			secondary_color = "#550055";
+			tertiary_color = "#CCAACC";
+			break;
+	}
+	document.body.style.setProperty('--primary-color', primary_color);
+	document.body.style.setProperty('--secondary-color', secondary_color);
+	document.body.style.setProperty('--tertiary-color', tertiary_color);
+}
+
 $(document).ready(function () {
 });
