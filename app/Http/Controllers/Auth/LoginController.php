79a68f6c2dcad1f3d8243504893cd012537d25ed
Maykon-Michel <maykoncti@gmail.com>
2017-08-02 15:06:24 -0300

    Login corrigido

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,67 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+    namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Foundation\Auth\AuthenticatesUsers;
+    use Illuminate\Http\Request;
 
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+    class LoginController extends Controller {
+        /*
+        |--------------------------------------------------------------------------
+        | Login Controller
+        |--------------------------------------------------------------------------
+        |
+        | This controller handles authenticating users for the application and
+        | redirecting them to your home screen. The controller uses a trait
+        | to conveniently provide its functionality to your applications.
+        |
+        */
 
-    use AuthenticatesUsers;
+        use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+        /**
+         * Where to redirect users after login.
+         *
+         * @var string
+         */
+        protected $redirectTo = '/';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+        /**
+         * Create a new controller instance.
+         *
+         * @return void
+         */
+        public function __construct() {
+            $this->middleware('guest')->except('logout');
+        }
+
+        public function showLoginForm() {
+            return view('index');
+        }
+
+        public function login(Request $request) {
+            $this->validateLogin($request);
+
+            if ($this->attemptLogin($request)) {
+                return $this->sendLoginResponse($request);
+            }
+
+            return $this->sendFailedLoginResponse($request);
+        }
+
+        protected function sendLoginResponse(Request $request) {
+            $request->session()->regenerate();
+
+            return $this->authenticated($request, $this->guard()->user())
+                ?: redirect()->intended($this->redirectPath());
+        }
+
+        protected function credentials(Request $request) {
+            return [
+                filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'matricula'
+                => $request->email,
+                'password' => $request->password,
+            ];
+        }
     }
-}
