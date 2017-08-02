a8907a2689ac9fc3399cac0d72b4cc822752e120
Maykon-Michel <maykoncti@gmail.com>
2017-08-02 15:16:42 -0300

    Revert "Revert "Login corrigido""
    This reverts commit 061a8628c2e04266df557e11ea52d0fd2cc81ae8.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,7 +6,7 @@
 
     class PagesController extends Controller {
         public function __construct() {
-            $this->middleware('auth');
+            $this->middleware('auth')->except('index');
         }
 
         public function index() {
