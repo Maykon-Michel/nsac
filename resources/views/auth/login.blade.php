d4953d792f8deaaffdaded9306ca3b6b90e6e727
Maykon-Michel <maykoncti@gmail.com>
2017-08-11 19:00:41 -0300

    Mensagem de erro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,28 +2,23 @@
     <div class="navbar-header">
         <h1 class="navbar-brand">Login</h1>
     </div>
-    <div class="container-fluid">
-        <form action="{{ route('login')  }}" method="POST" id="frm-login">
-            {{ csrf_field() }}
-            <div class="input-group">
-                <label for="ra" class="input-group-addon"><i class="glyphicon glyphicon-user"></i></label>
-                <input type="text" name="email" class="form-control" id="email" placeholder="R.A.">
-            </div>
-            @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-            @endif
-            <div class="input-group">
-                <label for="senha" class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></label>
-                <input type="text" name="password" class="form-control" id="password" placeholder="Senha">
-            </div>
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
-            <input type="submit" class="form-control" value="Entrar">
-        </form>
-    </div>
+    <form action="{{ route('login')  }}" method="POST" id="frm-login">
+        {{ csrf_field() }}
+        <div class="input-group">
+            <label for="ra" class="input-group-addon"><i class="glyphicon glyphicon-user"></i></label>
+            <input type="text" name="email" class="form-control" id="email" placeholder="R.A.">
+        </div>
+        <div class="input-group">
+            <label for="senha" class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></label>
+            <input type="text" name="password" class="form-control" id="password" placeholder="Senha">
+        </div>
+        <input type="submit" class="btn btn-block btn-raised btn-primary" value="Entrar">
+    </form>
+    @if($errors->has('email') || $errors->has('password'))
+        <div class="alert alert-dismissible alert-danger">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+            <p>{{ $errors->first('email') }}</p>
+            <p>{{ $errors->first('password') }}</p>
+        </div>
+    @endif
 </nav>
