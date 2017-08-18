91285221273200c662f9029cadfa22cf5478e2e6
Maykon-Michel <maykoncti@gmail.com>
2017-08-18 09:20:30 -0300

    Perfil
    Informações carregadas

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -14,16 +14,15 @@
                 <h1>Dados pessoais</h1>
                 <div class="form-group label-floating">
                     <label for="nome" class="control-label">Nome</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="nome"
                            name="nome"
-                           value="{{ Auth::user()->dado->nome }}">
+                           value="{{ Auth::user()->dado->nome }}"
+                           disabled>
                 </div>
                 <div class="form-group label-floating">
                     <label for="apelido" class="control-label">Apelido</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="apelido"
                            name="apelido"
                            value="{{ Auth::user()->dado->apelido }}">
@@ -32,38 +31,40 @@
                     <label class="control-label">Sexo</label>
                     <div class="radio">
                         <label>
-                            <input type="radio" name="sample1" value="option1" checked="">
+                            <input type="radio" name="sexo" value="0" {{ Auth::user()->dado->sexo ?: 'checked' }}>
                             Masculino
                         </label>
                     </div>
                     <div class="radio">
                         <label>
-                            <input type="radio" name="sample1" value="option1">
+                            <input type="radio" name="sexo" value="1" {{ Auth::user()->dado->sexo ? 'checked' : '' }}>
                             Feminino
                         </label>
                     </div>
                 </div>
                 <div class="form-group label-floating">
                     <label for="nome_pai" class="control-label">Nome do pai</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="nome_pai"
-                           name="nome_pai">
+                           name="nome_pai"
+                           value="{{ Auth::user()->dado->getPai->nome }}"
+                           disabled>
                 </div>
                 <div class="form-group label-floating">
                     <label for="nome_mae" class="control-label">Nome da mãe</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="nome_mae"
-                           name="nome_mae">
+                           name="nome_mae"
+                           value="{{ Auth::user()->dado->getMae->nome }}"
+                           disabled>
                 </div>
                 <div class="form-group label-floating">
                     <label for="data_de_nascimento" class="control-label">Data de nascimento</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="data_de_nascimento"
                            name="data_de_nascimento"
-                           value="{{ Auth::user()->dado->data_de_nascimento }}">
+                           value="{{ date("d/m/Y", strtotime(Auth::user()->dado->data_de_nascimento)) }}"
+                           disabled>
                 </div>
                 <div class="form-group">
                     <label for="cidade_nascimento">Cidade de nascimento</label>
@@ -79,60 +80,82 @@
                 </div>
                 <div class="form-group label-floating">
                     <label for="rg" class="control-label">R.G.</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="rg"
                            name="rg"
-                           maxlength="12">
+                           value="{{ Auth::user()->dado->rg }}"
+                           disabled>
                 </div>
                 <div class="form-group label-floating">
                     <label for="emissao_do_rg" class="control-label">Emissão do R.G.</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="emissao_do_rg"
-                           name="emissao_do_rg">
+                           name="emissao_do_rg"
+                           value="{{ date("d/m/Y", strtotime(Auth::user()->dado->emissao_do_rg)) }}"
+                           disabled>
                 </div>
             </div>
             <div class="col-md-6">
                 <h1>Dados de endereço</h1>
                 <div class="form-group label-floating">
                     <label for="logradouro" class="control-label">Logradouro</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="logradouro"
-                           name="logradouro">
+                           name="logradouro"
+                           value="{{ Auth::user()->dado->getEndereco->logradouro }}">
                 </div>
                 <div class="form-group label-floating">
                     <label for="numero" class="control-label">Número</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="numero"
-                           name="numero">
+                           name="numero"
+                           value="{{ Auth::user()->dado->getEndereco->numero }}">
                 </div>
                 <div class="form-group label-floating">
                     <label for="complemento" class="control-label">Complemento</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="complemento"
-                           name="complemento">
+                           name="complemento"
+                           value="{{ Auth::user()->dado->getEndereco->complemento }}">
                 </div>
                 <div class="form-group label-floating">
                     <label for="bairro" class="control-label">Bairro</label>
-                    <input type="text"
-                           class="form-control"
+                    <input class="form-control"
                            id="bairro"
-                           name="bairro">
+                           name="bairro"
+                           value="{{ Auth::user()->dado->getEndereco->bairro }}">
                 </div>
                 <div class="form-group">
-                    <label for="cidade">Cidade</label>
-                    <select name="cidade" class="form-control" id="cidade">
-                        <option value="Bauru">Bauru</option>
+                    <label for="pais">País</label>
+                    <select name="pais" class="form-control" id="pais">
+                        @foreach(\App\Pais::orderBy('nome')->get() as $pais)
+                            <option value="{{ $pais->codigo }}"
+                                    {{ $pais->codigo==Auth::user()->dado->getEndereco->pais->codigo ? 'selected' : '' }}>
+                                {{ $pais->nome }}
+                            </option>
+                        @endforeach
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="uf">U.F.</label>
                     <select name="uf" class="form-control" id="uf">
-                        <option value="SP">SP</option>
+                        @foreach(\App\Estado::orderBy('uf')->get()->where('pais', '=', Auth::user()->dado->getEndereco->pais->codigo) as $estado)
+                            <option value="{{ $estado->codigo }}"
+                                    {{ $estado->codigo==Auth::user()->dado->getEndereco->estado->codigo ? 'selected' : '' }}>
+                                {{ $estado->uf }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="cidade">Cidade</label>
+                    <select name="cidade" class="form-control" id="cidade">
+                        @foreach(\App\Cidade::orderBy('nome')->get()->where('estado', '=', Auth::user()->dado->getEndereco->estado->codigo) as $cidade)
+                            <option value="{{ $cidade->codigo }}"
+                                    {{ $cidade->codigo==Auth::user()->dado->getEndereco->cidade ? 'selected' : '' }}>
+                                {{ $cidade->nome }}
+                            </option>
+                        @endforeach
                     </select>
                 </div>
             </div>
